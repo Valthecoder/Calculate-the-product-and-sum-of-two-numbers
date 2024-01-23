8edcea1c18f0f82781bb0eb9316c32bdd01fa9f1
ValtheCoder <keshia.kdf@gmail.com>
2024-01-23 11:16:01 +0800

    Variables and If-Else Function

--- CalculatePS.py
+++ CalculatePS.py
@@ -1,1 +1,9 @@
-#
+# Set Variables
+def multiply_or_sum (num1,num2):
+# If and Else Function
+    if num1 * num2 <= 1000:
+        return num1 * num2
+    else:
+        return num1 + num2
+# Given number1 = 20 number2 = 30
+# Given number1 = 40 number2 = 30
