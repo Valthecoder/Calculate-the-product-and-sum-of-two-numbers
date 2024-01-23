09501ea9c3b37eab8c3d3f59703922e5911d967a
ValtheCoder <keshia.kdf@gmail.com>
2024-01-23 11:18:52 +0800

    For 20 and 30

--- CalculatePS.py
+++ CalculatePS.py
@@ -1,9 +1,14 @@
 # Set Variables
-def multiply_or_sum (num1,num2):
-# If and Else Function
+def multiply_or_sum(num1, num2):
+    # If and Else Function
     if num1 * num2 <= 1000:
         return num1 * num2
     else:
         return num1 + num2
+
+
 # Given number1 = 20 number2 = 30
+result = multiply_or_sum(20, 30)
+print("The result is", result)
+
 # Given number1 = 40 number2 = 30
